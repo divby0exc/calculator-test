dd45fec32219c03aa52dfaff1b70f824bf491e91
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 11:55:56 +0200

    revert back to return 0 on the methods

--- src/main/java/com/divby0exc/calculatortestspring/model/Calculator.java
+++ src/main/java/com/divby0exc/calculatortestspring/model/Calculator.java
@@ -3,10 +3,12 @@ package com.divby0exc.calculatortestspring.model;
 public class Calculator {
 
     public static int add(int a, int b) {
-        return a+b;
+//        return a+b;
+        return 0;
     }
 
     public static int sub(int a, int b) {
-        return a-b;
+//        return a-b;
+        return 0;
     }
 }
