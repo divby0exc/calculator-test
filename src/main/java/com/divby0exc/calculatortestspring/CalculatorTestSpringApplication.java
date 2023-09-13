033d4e8d0cd6cdf5771f5c5d9e5cdd4983d6f9f0
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 11:14:18 +0200

    first commit

---
+++ src/main/java/com/divby0exc/calculatortestspring/CalculatorTestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.divby0exc.calculatortestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorTestSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/divby0exc/calculatortestspring/CalculatorTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divby0exc.calculatortestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
